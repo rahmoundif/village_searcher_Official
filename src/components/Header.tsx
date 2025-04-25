ba2c8a310de0167e9a8d4e5159017e06e4e0e1f1
rahmoundif <rahmoun.dif@gmail.com>
2025-04-25 16:02:36 +0200

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
 	return ( 
 	<div className="bg-[#F9EDDA] rounded-b-xl p-5 flex justify-center w-full ">
-		<img src="./src/assets/LogoVilles.png" alt="logo" className="w-30 lg:w-50" />
+		<img src="/LogoVilles.png" alt="logo" className="w-30 lg:w-50" />
 	</div> );
 }
 
